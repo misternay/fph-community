a1cf31c2ca7304eab791e860517c33b5e4030fbe
MisterNay <misternay@odds.team>
2018-12-25 08:21:14 +0700

    Update - group date of missing people

--- src/pages/missing-list/missing-list.ts
+++ src/pages/missing-list/missing-list.ts
@@ -26,46 +26,54 @@ export interface FirebaseResponse {
 export class MissingListPage {
 
     datelist = ['today', 'yesterday', 'monthago']
+    monthList = ['ธันวาคม', 'พฤศจิกายน', 'ตุลาคม', 'อื่นๆ']
+
     listDateItem = this.getMockMissingList.getMockData();
-    // listItem: Array<MissingList>;
+
+    listPeople = {
+        december: [],
+        november: [],
+        october: [],
+        other: []
+    }
     people = [];
+
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
         private getMockMissingList: MockMissingList,
         private dialogUtil: DialogUtilService,
     ) {
-        this.getListOfPeople();
     }
 
     ionViewDidLoad() {
+        if (localStorage.getItem('peopleList')) {
+            this.people = JSON.parse(localStorage.getItem('peopleList'));
+            this.people.forEach((value: { date: string, name: string, image: string, detail: string }) => {
+                if (value.date.includes(this.monthList[0])) {
+                    this.listPeople.december.push(value)
+                } else if (value.date.includes(this.monthList[1])) {
+                    this.listPeople.november.push(value)
+                } else if (value.date.includes(this.monthList[2])) {
+                    this.listPeople.october.push(value)
+                } else {
+                    this.listPeople.other.push(value)
+                }
+            })
+        } else {
+            this.getListOfPeople();
+        }
     }
 
     ionViewDidEnter() {
-        // this.getFromFirebase();
     }
 
     goTOSearchDetail(dayIndex: number, index: number) {
-        console.log(dayIndex, index)
         this.navCtrl.push('SearchPage', {
             missingData: this.listDateItem[index]
         });
     }
 
-    private getFromFirebase() {
-        const ref = firebase.database().ref('/');
-
-        ref.on('value', resp => {
-            // let list = [];
-
-            // resp.forEach(res => {
-            //     list.push(res.val())
-            // })
-
-            this.listDateItem = resp.val();
-            this.dialogUtil.hideLoadingDialog();
-        })
-    }
     getListOfPeople() {
         this.dialogUtil.showLoadingDialog();
         var otherDatabase = otherApp.database().ref("/peopleList");
@@ -73,6 +81,7 @@ export class MissingListPage {
             resp.forEach(va => {
                 console.log(va.val())
                 this.people.push(va.val())
+                localStorage.setItem('peopleList', JSON.stringify(this.people));
             })
             this.dialogUtil.hideLoadingDialog();
         });
